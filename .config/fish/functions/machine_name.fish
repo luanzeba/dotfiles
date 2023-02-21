2bc715e40a15388dad81101fc4eefa0695417551
Luan Vieira <luanzeba@github.com>
2023-02-21 17:23:58 -0500

    Add spacing

--- .config/fish/functions/machine_name.fish
+++ .config/fish/functions/machine_name.fish
@@ -1,7 +1,7 @@
 function machine_name -d "returns current machine name"
   if test -e ~/.friendly_name
     # codespaces friendly name
-    echo -ns ' 💻' (cat ~/.friendly_name)
+    echo -ns ' 💻 ' (cat ~/.friendly_name)
   else
     echo -ns '@' (hostname)
   end
