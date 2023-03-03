e8bdbfd307e47a275ad23d4875059d71e86d5a9a
Luan Vieira <luanzeba@github.com>
2023-03-03 17:12:39 -0500

    That didn't work

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,7 +24,6 @@ export PATH="$PATH:$HOME/bin"
 export BAT_THEME="GitHub"
 
 if test $CODESPACES
-  export PWD="/workspaces/$RepositoryName"
   export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin"
   if test -d /workspaces/github
     export PATH="$PATH:/workspaces/github/bin"
