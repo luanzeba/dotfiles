e3e67a88c33eaffc10f4a077eab558246cc0e71c
Luan Vieira <luanzeba@github.com>
2025-10-23 11:40:17 -0400

    Improve git nvim support

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -4,6 +4,9 @@ return {
 		keys = {
 			{ "]h", "<Plug>(GitGutterNextHunk)", desc = "Go to next hunk" },
 			{ "[h", "<Plug>(GitGutterPrevHunk)", desc = "Go to previous hunk" },
+			{ "<leader>ghs", "<Plug>(GitGutterStageHunk)", desc = "Stage hunk" },
+			{ "<leader>ghu", "<Plug>(GitGutterUndoHunk)", desc = "Undo hunk" },
+			{ "<leader>ghp", "<Plug>(GitGutterPreviewHunk)", desc = "Preview hunk" },
 		},
 	},
 
@@ -13,6 +16,8 @@ return {
 		keys = {
 			{ "<leader>gg", "<cmd>Git<CR>5j", desc = "Git status" },
 			{ "<leader>gr", "<cmd>Gread<CR>", desc = "Read file from git" },
+			{ "<leader>gd", "<cmd>Gvdiffsplit<CR>", desc = "Vertical diff split" },
+			{ "<leader>gD", "<cmd>Gdiffsplit<CR>", desc = "Horizontal diff split" },
 		},
 	},
 }
