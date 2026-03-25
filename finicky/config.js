8d356340e54e7a74078450105f47683f429c54cf
Luan Vieira <luanzeba@github.com>
2026-03-25 15:00:57 -0400

    Add Finicky install and routing config

---
+++ finicky/config.js
@@ -0,0 +1,31 @@
+// @ts-check
+
+/**
+ * @typedef {import('/Applications/Finicky.app/Contents/Resources/finicky.d.ts').FinickyConfig} FinickyConfig
+ */
+
+/** @type {FinickyConfig} */
+export default {
+  defaultBrowser: {
+    name: "Google Chrome",
+    profile: "Work",
+  },
+  handlers: [
+    {
+      // GitHub org URLs should always open in the Work Chrome profile.
+      match: /^https:\/\/github\.com\/github(?:[/?#]|$)/,
+      browser: {
+        name: "Google Chrome",
+        profile: "Work",
+      },
+    },
+    {
+      // X should always open in the Home Chrome profile.
+      match: /^https:\/\/x\.com(?:[/?#]|$)/,
+      browser: {
+        name: "Google Chrome",
+        profile: "Home",
+      },
+    },
+  ],
+};
