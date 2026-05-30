431ababf7c58c032382ceb62aed93ed603783f29
Luan Vieira <luanv@me.com>
2026-05-30 19:23:32 -0400

    nvim: show absolute line numbers

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -3,3 +3,7 @@ vim.g.maplocalleader = "\\"
 
 -- Keep defaults unless we have a clear reason to change them.
 vim.opt.termguicolors = true
+
+-- Always show absolute line numbers.
+vim.opt.number = true
+vim.opt.relativenumber = false
