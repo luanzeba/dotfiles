9fb0ae4ceed5225d32bc865d0a62629734d561dd
Luan Vieira <luanv@me.com>
2026-05-31 21:17:15 -0400

    nvim: set buffer navigation mappings

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,2 +1,6 @@
 -- Keep this file for global/editor-level mappings only.
 -- Plugin-specific mappings should live with each plugin config.
+
+vim.keymap.set("n", "]b", "<cmd>bnext<cr>", { desc = "Buffer: Next" })
+vim.keymap.set("n", "[b", "<cmd>bprevious<cr>", { desc = "Buffer: Previous" })
+vim.keymap.set("n", "ga", "<cmd>buffer #<cr>", { desc = "Buffer: Alternate" })
